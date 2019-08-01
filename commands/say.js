449716187a31ea8d333ca9f49942fb5ccbe71103
lolifaggot <53565588+lolifaggot@users.noreply.github.com>
2019-08-01 21:16:47 +0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 const discord = require("discord.js");;
 const config = require("../config.json");
+
 module.exports.run = async (bot, message, args) => {
     if(message.author.id != "263228336036118531") return message.channel.send("S-Sorry, you are not my owner!");
 
@@ -14,7 +15,7 @@ message.channel.send(sayMessage)
 module.exports.help = {
   name: "say",
   aliases: [],
-  usage: "?say <message>",
+  usage: ".say <message>",
   description: "Say a message.",
   accessableby: "Members",
   noalias: "No Aliases"
