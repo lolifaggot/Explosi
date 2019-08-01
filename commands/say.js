96c65364ecd0a20b8e90c7bf2ef02524b31ec876
lolifaggot <53565588+lolifaggot@users.noreply.github.com>
2019-08-01 21:09:56 +0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,18 +1,7 @@
-exports.run = (client, message) => {
-let args = message.content.split(" ").slice(1);
-message.delete();
-if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!");
-message.channel.send(args.join(" "));
-};
-exports.conf = {
-enabled: true,
-guildOnly: false,
-aliases: [],
-permLevel: 0
-};
-exports.help = {
-  if(message.author.id != "263228336036118531") return message.channel.send("S-Sorry, you are not my owner!");
-name: "say",
-description: "Makes the bot repeat your message.",
-usage: "say [message]"
-};
+if(message.author.id != "263228336036118531") return message.channel.send("S-Sorry, you are not my owner!");
+
+let sayMessage = args.join(" ");
+
+message.delete().catch()
+
+message.channel.send(sayMessage)
