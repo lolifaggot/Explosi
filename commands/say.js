55832c83f8be949bdc6827a2e732642586a1e326
lolifaggot <53565588+lolifaggot@users.noreply.github.com>
2019-08-01 21:05:26 +0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -11,6 +11,7 @@ aliases: [],
 permLevel: 0
 };
 exports.help = {
+  if(message.author.id != "263228336036118531") return message.channel.send("S-Sorry, you are not my owner!");
 name: "say",
 description: "Makes the bot repeat your message.",
 usage: "say [message]"
