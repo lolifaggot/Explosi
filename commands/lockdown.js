5b551d97074294be68f04970c958e6bcd0b4672b
lolifaggot <53565588+lolifaggot@users.noreply.github.com>
2019-08-02 13:09:50 +0400

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,19 +1,52 @@
+const ms = require('ms');
 exports.run = (client, message, args) => {
-if (!client.lockit) client.lockit = [];
-if (!message.member.hasPermission("MANAGE_CHANNELS")) return msg.reply("❌**Error:** You don't have the permission to do that!");
-message.channel.overwritePermissions(channel.guild.id, {
-SEND_MESSAGES: false
-})
-message.channel.send(`Damnn, **${message.author.username}** just locked the channel down. Don't worry, Admins will soon open the chat again so be patient.`);
+    if (!client.lockit) client.lockit = [];
+    let time = args.join(' ');
+    let validUnlocks = ['release', 'unlock'];
+    let modRole = message.guild.roles.find('name', 'delet Mod');
+    if (!message.member.roles.has(modRole.id)) {
+        return message.reply('you have insufficient permissions to use this command.').catch(console.error);
+    }
+    if (!time) return message.reply('you must set a duration for the lockdown in either hours, minutes or seconds.');
+
+    if (validUnlocks.includes(time)) {
+        message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+        }).then(() => {
+            message.channel.sendMessage('**Lockdown lifted.**');
+            clearTimeout(client.lockit[message.channel.id]);
+            delete client.lockit[message.channel.id];
+        }).catch(error => {
+            console.log(error);
+        });
+    } else {
+        message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+        }).then(() => {
+            message.channel.send(`**Channel locked** for ${ms(ms(time), { long:true })}.`).then(() => {
+
+                client.lockit[message.channel.id] = setTimeout(() => {
+                    message.channel.overwritePermissions(message.guild.id, {
+                        SEND_MESSAGES: null
+                    }).then(message.channel.send('**Lockdown lifted.**')).catch(console.error);
+                    delete client.lockit[message.channel.id];
+                }, ms(time));
+
+            }).catch(error => {
+                console.log(error);
+            });
+        });
+    }
 };
 exports.conf = {
-enabled: true,
-guildOnly: false,
-aliases: ['ld'],
-permLevel: 0
+    enabled: true,
+    guildOnly: true,
+    aliases: ['ld', 'lock'],
+    permLevel: 0
 };
+
 exports.help = {
-name: 'lockdown',
-description: 'This will lock a channel down.',
-usage: 'lockdown'
+    name: 'lockdown',
+    description: 'Locks a channel for a set duration (in hrs, mins or secs).',
+    usage: 'lockdown <duration>'
 };
