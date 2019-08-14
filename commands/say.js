c2484fbfbe18d88a3eddb41618ddf7358966b3b2
lolifaggot <53565588+lolifaggot@users.noreply.github.com>
2019-08-14 10:58:27 +0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,7 @@ const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.id != "263228336036118531") return message.channel.send("S-Sorry, you are not my owner!");
+     if(message.author.id != "497227982382039051") return message.channel.send("S-Sorry, you are not my owner!");
 
 let sayMessage = args.join(" ");
 
